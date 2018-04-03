aa4ad93775f7e238274dd24431ef2b08ee397c65
Courtney Ann Shearer <csheare@ada2.cs.clemson.edu>
2018-04-03 12:55:27 -0400

    fixed bug

--- projects/4/tracker/multisprite.cpp
+++ projects/4/tracker/multisprite.cpp
@@ -2,16 +2,6 @@
 #include "gamedata.h"
 #include "renderContext.h"
 
-Vector2f MultiSprite::makeVelocity(int vx, int vy) const {
-
-  float newvx = Gamedata::getInstance().getRandFloat(vx-50,vx+50);;
-  float newvy = Gamedata::getInstance().getRandFloat(vy-50,vy+50);;
-  newvx *= [](){ if(rand()%2) return -1; else return 1; }();
-  newvy *= [](){ if(rand()%2) return -1; else return 1; }();
-  std::cout<<newvx<<" "<<newvy <<std::endl;
-  return Vector2f(newvx, newvy);
-}
-
 
 void MultiSprite::advanceFrame(Uint32 ticks) {
 	timeSinceLastFrame += ticks;
@@ -25,7 +15,7 @@ MultiSprite::MultiSprite( const std::string& name) :
   Drawable(name,
            Vector2f(Gamedata::getInstance().getXmlInt(name+"/startLoc/x"),
                     Gamedata::getInstance().getXmlInt(name+"/startLoc/y")),
-           makeVelocity(Gamedata::getInstance().getXmlInt(name+"/speedX"),
+           Vector2f(Gamedata::getInstance().getXmlInt(name+"/speedX"),
                     Gamedata::getInstance().getXmlInt(name+"/speedY"))
            ),
   images( RenderContext::getInstance()->getImages(name) ),
